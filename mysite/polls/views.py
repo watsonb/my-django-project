cebf010a26b3a5c57df00024b8df386331e8d2fb
Ben Watson <bwatson@mitre.org>
2016-03-01 08:35:07 -0600

    fixing object get

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -17,7 +17,7 @@ class IndexView(generic.ListView):
         published in the future).
         """
         #return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
-        return Question.objects[:5]
+        return Question.objects.all()[:5]
 
 class DetailView(generic.DetailView):
     model = Question
